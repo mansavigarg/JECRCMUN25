7215c4dc771408be94b84ef56d38298fd9e1735b
Antriksh005 <145842504+Antriksh005@users.noreply.github.com>
2025-04-04 22:19:01 +0530

    Team update

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,29 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
-import Card3DWave from "../components/ui/Card3DWave";
-import teamData from "../teamData/team.json";
+import teamData from "/public/team.json";
 import bannerImg from "../assets/banner2.jpg";
-
+import TeamSection from "../components/ui/TeamSection ";
 function Team() {
-  const cardVariants = {
-    hidden: { opacity: 0, y: 100 },
-    visible: (i) => ({
-      opacity: 1,
-      y: 0,
-      transition: {
-        delay: i * 0.1,
-        duration: 0.9,
-        ease: "easeOut",
-        type: "spring",
-        stiffness: 80,
-      },
-    }),
-  };
-
   return (
     <div className="min-h-screen bg-white flex flex-col">
       <div
-        className="w-full h-[40vh] flex flex-col items-center justify-center"
+        className="w-full h-[30vh] sm:h-[28vh] flex flex-col items-center justify-center"
         style={{
           backgroundImage: `url(${bannerImg})`,
           backgroundSize: "cover",
@@ -32,39 +16,16 @@ function Team() {
         }}
       >
         <motion.h1
-          className="text-6xl md:text-8xl font-bold text-[#991C1C] tracking-wide uppercase shadow-lg"
-          initial={{ opacity: 0, scale: 0.7 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 1, ease: "easeOut" }}
+          className="text-4xl sm:text-6xl md:text-7xl font-bold text-[#991C1C] tracking-wide uppercase shadow-lg"
         >
           JECRCMUN
         </motion.h1>
-        <h2 className="text-4xl font-bold text-[#000000] mt-4">Our Team</h2>
+        <h2 className="text-2xl sm:text-4xl font-bold text-[#000000] mt-2 sm:mt-4">Our Team</h2>
       </div>
 
-      <div className="flex flex-col items-center py-5 px-4">
-  <div className="space-y-16 max-w-7xl w-full">
-    {teamData.map((vertical, index) => (
-      <div key={index} className="flex flex-col items-center">
-        <h2 className="text-3xl font-bold mb-4 text-gray-800">{vertical.title}</h2>
-        <div className="flex flex-wrap justify-center gap-15 w-full">
-          {vertical.members.map((member, memberIndex) => (
-            <motion.div
-              key={memberIndex}
-              custom={memberIndex}
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true, amount: 0.1 }}
-              variants={cardVariants}
-            >
-              <Card3DWave member={member} cardColor="#060807" accentColor="#991C1C" />
-            </motion.div>
-          ))}
-        </div>
+      <div className="flex flex-col items-center py-2 px-1 sm:py-4 sm:px-2">
+        <TeamSection teamData={teamData} />
       </div>
-    ))}
-  </div>
-</div>
     </div>
   );
 }
